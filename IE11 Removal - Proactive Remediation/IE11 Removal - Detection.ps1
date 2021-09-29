4f11c339c195597b98980dbf47377b43d129e308
Nicklas Ahlberg <74212904+NicklasAhlberg@users.noreply.github.com>
2021-09-29 07:28:13 +0200

    Add files via upload

---
+++ IE11 Removal - Proactive Remediation/IE11 Removal - Detection.ps1
@@ -0,0 +1,25 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	2021-09-28
+	 Created by:   	Nicklas Ahlberg
+	 Blog: 			https://nicklasahlberg.se
+	 Filename:     	IE11 Removal - Detection.ps1
+	===========================================================================
+	.DESCRIPTION
+		Delivered as-is with no warranties or guarantees
+#>
+
+$IEexist = Get-WindowsCapability -Online -Name Browser.InternetExplorer~~~~0.0.11.0
+
+if ($IEexist.State -eq 'Installed')
+{
+	Write-Host "IE11 is installed and should be remidiated"
+	Exit 1
+}
+
+else
+{
+	Write-Host "IE11 is not installed remediation is not needed"
+	Exit 0
+}
---
+++ IE11 Removal - Proactive Remediation/IE11 Removal - Remediation.ps1
@@ -0,0 +1,13 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	2021-09-28
+	 Created by:   	Nicklas Ahlberg
+	 Blog: 			https://nicklasahlberg.se
+	 Filename:     	IE11 Removal - Remediation.ps1
+	===========================================================================
+	.DESCRIPTION
+		Delivered as-is with no warranties or guarantees
+#>
+
+Remove-WindowsCapability -Online -Name Browser.InternetExplorer~~~~0.0.11.0
