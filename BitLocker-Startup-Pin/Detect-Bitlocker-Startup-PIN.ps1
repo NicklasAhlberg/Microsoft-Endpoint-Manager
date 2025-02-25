fe725cff9192e0dd3bcea08a48ba997222c39da0
Nicklas Ahlberg <74212904+NicklasAhlberg@users.noreply.github.com>
2025-02-25 15:02:43 +0100

    Add files via upload

---
+++ BitLocker-Startup-Pin/Detect-Bitlocker-Startup-PIN.ps1
@@ -0,0 +1,43 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	2021-11-13
+	 Updated on:   	2024-10-23
+	 Created by:   	Nicklas Ahlberg
+	 Organization: 	rockenroll.tech
+	 Filename:     	Detect-Bitlocker-Startup-PIN.ps1
+	 Version:       2024.10.23
+	===========================================================================
+	.DESCRIPTION
+		Use this script in Proactive Remediation to detect wether a Bitlocker startup PIN has been set to the device or not.
+	.WARRANTY
+		The tool is provided "AS IS" with no warranties
+#>
+
+# Make sure the BitLocker Startup PIN Tool is not already running (overlapPINg schedule)
+$bitLockerToolProcess = Get-Process -Name Bitlocker-Startup-PIN-Tool -ErrorAction SilentlyContinue
+if (! $bitLockerToolProcess) {
+	
+	# Get Bitlocker status
+	$BitLocker = Get-BitLockerVolume -MountPoint $env:SystemDrive
+	if ($BitLocker.VolumeStatus -ne 'FullyDecrypted') {
+		if ($BitLocker.KeyProtector.KeyProtectorType -notcontains 'TPMPIN') {
+			Write-Host "OS-disk is encrypted but startup PIN was not found. Remediation is needed"
+			Exit 1
+		}
+		
+		else {
+			Write-Host "OS-disk is encrypted and startup PIN was found. No need to remediate"
+			Exit 0
+		}
+		
+	}
+	
+	else {
+		Exit 0 # Exit if Bitlocker is not enabled on the device
+	}
+}
+
+else {
+	Exit 0 # Exit if the tool process is already running
+}
---
+++ BitLocker-Startup-Pin/Remediate-Bitlocker-Startup-PIN.ps1
@@ -0,0 +1,59 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created on:   	2021-11-13
+ 	 Updated on:   	2024-10-23
+	 Created by:   	Nicklas Ahlberg
+	 Organization: 	rockenroll.tech
+	 Filename:     	Remediate-Bitlocker-Startup-PIN.ps1
+	 Version:       2024.10.23
+	===========================================================================
+	.DESCRIPTION
+		Use this script in Proactive Remediation to download the tool from an Azure Storage account
+	.WARRANTY
+		The tool is provided "AS IS" with no warranties
+#>
+
+# Create log directory, file and function
+$logFolderLocation = 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs'
+if (!(Test-Path $logFolderLocation) -eq $true) { New-Item -Path $logFolderLocation -ItemType Dir -ErrorAction Ignore }
+$LogFile = "$logFolderLocation\Bitlocker-Startup-PIN.log"
+
+Function Write-Log {
+	param (
+		$Log
+	)
+	
+	$TimeStamp = "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+	Add-Content $LogFile  "$TimeStamp - $Log"
+}
+
+Write-Log -Log "Remediation script started"
+
+Try {
+	# Declare variables
+	$DownloadURL = "Your SAS URL goes here"
+	$ZIP_File = "C:\Windows\temp\Bitlocker-Startup-PIN-Tool.zip" # ZIP-file download location
+	$ExtractedFolder = 'C:\Windows\Temp\Bitlocker-Startup-PIN-Tool' # Location to the extraced ZIP-file
+	
+	# Download the .ZIP-file from storeage account
+	Invoke-WebRequest -Uri $DownloadURL -OutFile $ZIP_File
+	
+	# Extract the .ZIP-file
+	Expand-Archive -Path $ZIP_File -DestinationPath $ExtractedFolder -Force
+	
+	# Execute 
+	Try {
+		cmd /c start /WAIT "$ExtractedFolder\ServiceUI.exe" -Process:explorer.exe "$ExtractedFolder\Bitlocker-Startup-PIN-Tool.exe"
+	}
+	
+	Catch {
+		Write-Log -Log "Something went wrong"; Write-Log -Log "$_.Exception.Message"; Exit 1
+	}
+	
+	Write-Log -Log "Remediation script has completed successfully"; Exit 0
+}
+
+catch {
+	Write-Log -Log "Something went wrong"; Write-Log -Log "$_.Exception.Message"; Exit 1
+}
